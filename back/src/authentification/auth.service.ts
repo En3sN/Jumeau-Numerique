85fa250716a42462d0d1f3da5500117e4e1dfd5c
enes esen <enes.esen@uha.fr>
2024-06-04 10:45:58 +0200

    Feat : connexion avec l'attribut salt

--- back/src/authentification/auth.service.ts
+++ back/src/authentification/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { DataSource } from 'typeorm';
+import { DataSource, EntityManager } from 'typeorm';
 import { Utilisateur } from '../utilisateur/Entities/utilisateur.entity';
 import { TransactionManager } from '../Shared/TransactionManager/TransactionManager';
 import * as bcrypt from 'bcrypt';
@@ -12,26 +12,28 @@ export class AuthService {
     private dataSource: DataSource,
     private jwtService: JwtService,
     private transactionManager: TransactionManager
-  ) { }
+  ) {}
 
   async validateUser(email: string, pass: string): Promise<any> {
-    const user = await this.dataSource.getRepository(Utilisateur).findOne({ where: { email: email, activated: true } });
-    if (!user) {
-      throw new UnauthorizedException('Invalid credentials');
-    }
+    return await this.transactionManager.executeInTransaction(async (manager: EntityManager) => {
+      const user = await manager.getRepository(Utilisateur).findOne({ where: { email: email, activated: true } });
+      console.log(user);
+      if (!user) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+      const hashedPassword = await bcrypt.hash(pass, user.salt);
 
-    const isPasswordValid = await bcrypt.compare(pass, user.pwd);
-    
-    if (!isPasswordValid) {
-      throw new UnauthorizedException('Invalid credentials');
-    }
+      const isPasswordValid = hashedPassword === user.pwd;
 
-    return await this.transactionManager.executeInTransaction(async manager => {
+      if (!isPasswordValid) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+
       const sessionCode = await this.getUserSessionCode(manager, user.email, user.pwd);
       return { ...user, sessionCode };
     });
   }
-  
+
   async login(user: any) {
     const payload = {
       id: user.id,
@@ -43,7 +45,7 @@ export class AuthService {
     };
   }
 
-  private async getUserSessionCode(manager: any, email: string, password: string): Promise<string> {
+  private async getUserSessionCode(manager: EntityManager, email: string, password: string): Promise<string> {
     const query = `SELECT security.get_user_code($1, $2) AS session_code`;
     const params = [email, password];
     const result = await manager.query(query, params);
